328febe34ea486945acb1fc080c8d55cb8d14a21
jessicsous <desousa.jessicasilva1@gmail.com>
2022-05-24 10:42:39 -0400

    atualização das pastas

--- tripadvisor_teste/main.py
+++ tripadvisor_teste/main.py
@@ -2,48 +2,51 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.options import Options
 
+# entra no site
 options = Options()
 options.add_argument('window-size=1280,720')
 navegador = webdriver.Chrome(options=options)
 navegador.get('https://www.tripadvisor.com.br')
 
-# clica no botão 'Mais'
-primeiro_input = navegador.find_elements_by_tag_name('button')[-27]
-primeiro_input.click()
-sleep(1)
-
-# clica no botao 'Adicione um local'
-campo_pesquisa = navegador.find_element_by_id('menu-item-0')
-campo_pesquisa.click()
-sleep(2)
+pesquisas = ['Belo Horizonte']
+sleep(4)
 
 # clica no botão 'Aceitar'
-button_aceitar = navegador.find_element_by_id('onetrust-accept-btn-handler')
-button_aceitar.click()
-sleep(2)
+def primeiro_botao():
+    button_aceitar = navegador.find_element_by_id('onetrust-accept-btn-handler')
+    button_aceitar.click()
+    sleep(2)
 
-# pesquisa no search
-pesquisar = navegador.find_element_by_xpath('//*[@id="lithium-root"]/header/div/nav/div/div[1]/div/div/form/input[1]')
-pesquisar.click()
-pesquisar.send_keys('Belo Horizonte')
-pesquisar.submit()
-sleep(3)
+# faz a pesquisa
+def input(pesquisa):
+    primeiro_input = navegador.find_element_by_xpath('//input[contains(@placeholder, "Aonde você quer ir?")]')
+    primeiro_input.click()
+    primeiro_input.send_keys(pesquisa)
+    primeiro_input.submit()
+    sleep(3)
+
 
 # realiza web scraping
-cidade = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div/div[1]').text
-estado = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div[1]/div[2]').text
-descricao = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div[2]/div').text
-ponto_turistico = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]').text
+def raspagem():
+    cidade = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div/div[1]').text
+    estado = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div[1]/div[2]').text
+    descricao = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div[2]/div').text
+    ponto_turistico = navegador.find_element_by_xpath('//*[@id="BODY_BLOCK_JQUERY_REFLOW"]/div[2]/div/div[2]/div/div/div/div/div[1]/div/div[1]/div/div[3]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/div[1]').text
 
-navegador.close()
+    # guarda na TripAdvisor.txt
+    with open('tripadvisor.txt', 'w+') as arquivo:
+        arquivo.write(f'cidade: {cidade}\n')
+        arquivo.write(f'cidade: {estado}\n')
+        arquivo.write(f'cidade: {descricao}\n')
+        arquivo.write(f'cidade: {ponto_turistico}\n')
+        arquivo.close()
 
-# guarda na TripAdvisor.txt
-with open('tripadvisor.txt', 'w+') as arquivo:
-    arquivo.write(f'cidade: {cidade}\n')
-    arquivo.write(f'cidade: {estado}\n')
-    arquivo.write(f'cidade: {descricao}\n')
-    arquivo.write(f'cidade: {ponto_turistico}\n')
-    arquivo.close()
+for pesquisa in pesquisas:
+    primeiro_botao()
+    input(pesquisa)
+    raspagem()
+
+
 
 
             
--- yellowpages_teste/main.py
+++ yellowpages_teste/main.py
@@ -10,46 +10,51 @@ options.add_argument('window-size=1280,720')
 driver = webdriver.Chrome(options=options)
 driver.get('https://www.yellowpages.com/')
 
+servicos = ['destists']
+
 # faz a pesquisa
-pesquisa = driver.find_element_by_id('query')
-pesquisa.send_keys('destists')
-sleep(1)
-pesquisa_cidade = driver.find_element_by_id('location')
-pesquisa_cidade.clear()
-pesquisa_cidade.send_keys('Los Angeles, CA')
-pesquisa_cidade.submit()
-sleep(1)
+def pesquisa_servico(servico):
+    pesquisa = driver.find_element_by_id('query')
+    pesquisa.send_keys(servico)
+    pesquisa.submit()
+    sleep(2)
 
-# faz a raspagem
-telefone = driver.find_elements_by_xpath('//div[@class="phones phone primary"]')
-endereco = driver.find_elements_by_xpath('//div[@class="street-address"]')
-cidade = driver.find_elements_by_xpath('//div[@class="locality"]')
-comentario = driver.find_elements_by_xpath('//p[@class="body with-avatar"]')
+# captura os dados
+def faz_raspagem():
+    telefone = driver.find_elements_by_xpath('//div[@class="phones phone primary"]')
+    endereco = driver.find_elements_by_xpath('//div[@class="street-address"]')
+    cidade = driver.find_elements_by_xpath('//div[@class="locality"]')
+    comentario = driver.find_elements_by_xpath('//p[@class="body with-avatar"]')
 
-# primeiro serviço
-telefone1 = telefone[0].text
-endereco1 = endereco[0].text
-cidade1 = cidade[0].text
-comentario1 = comentario[0].text
+    # primeiro serviço
+    telefone1 = telefone[0].text
+    endereco1 = endereco[0].text
+    cidade1 = cidade[0].text
+    comentario1 = comentario[0].text
 
-# segundo serviço
-telefone2 = telefone[1].text
-endereco2 = endereco[1].text
-cidade2 = cidade[1].text
-comentario2 = comentario[1].text
+    # segundo serviço
+    telefone2 = telefone[1].text
+    endereco2 = endereco[1].text
+    cidade2 = cidade[1].text
+    comentario2 = comentario[1].text
 
-# terceiro serviço
-telefone3 = telefone[2].text
-endereco3 = endereco[2].text
-cidade3 = cidade[2].text
-comentario3 = comentario[2].text
+    # terceiro serviço
+    telefone3 = telefone[2].text
+    endereco3 = endereco[2].text
+    cidade3 = cidade[2].text
+    comentario3 = comentario[2].text
 
-primeiro_dado = (telefone1 + ';' + endereco1 + ';' + cidade1 + ';' + comentario1)
-segundo_dado = (telefone2 + ';' + endereco2 + ';' + cidade2 + ';' + comentario2)
-terceiro_dado = (telefone3 + ';' + endereco3 + ';' + cidade3 + ';' + comentario3)
+    primeiro_dado = (telefone1 + ';' + endereco1 + ';' + cidade1 + ';' + comentario1)
+    segundo_dado = (telefone2 + ';' + endereco2 + ';' + cidade2 + ';' + comentario2)
+    terceiro_dado = (telefone3 + ';' + endereco3 + ';' + cidade3 + ';' + comentario3)
 
-with open('yellowpages.txt', 'w+') as arquivo:
-    arquivo.write(primeiro_dado + '\n')
-    arquivo.write(segundo_dado + '\n')
-    arquivo.write(terceiro_dado + '\n')
-    arquivo.close()
+    # salva em arquivo .txt
+    with open('yellowpages.txt', 'w+') as arquivo:
+        arquivo.write(primeiro_dado + '\n')
+        arquivo.write(segundo_dado + '\n')
+        arquivo.write(terceiro_dado + '\n')
+        arquivo.close()
+
+for servico in servicos:
+    pesquisa_servico(servico)
+    faz_raspagem()
